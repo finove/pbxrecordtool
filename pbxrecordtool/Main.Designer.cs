bbda7ad51bd78bffb76ef3de64a796ffd99e8a4f
finove <finove@qq.com>
2020-04-01 10:12:54 +0800

    发送同步请求

---
+++ pbxrecordtool/Main.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace pbxrecordtool
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.btnStartUpdate = new System.Windows.Forms.Button();
+            this.txtDeviceName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDeviceMAC = new System.Windows.Forms.TextBox();
+            this.dtpUpdateDate = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartUpdate
+            // 
+            this.btnStartUpdate.Location = new System.Drawing.Point(315, 304);
+            this.btnStartUpdate.Name = "btnStartUpdate";
+            this.btnStartUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnStartUpdate.TabIndex = 0;
+            this.btnStartUpdate.Text = "开始同步";
+            this.btnStartUpdate.UseVisualStyleBackColor = true;
+            this.btnStartUpdate.Click += new System.EventHandler(this.btnStartUpdate_Click);
+            // 
+            // txtDeviceName
+            // 
+            this.txtDeviceName.Location = new System.Drawing.Point(313, 92);
+            this.txtDeviceName.Name = "txtDeviceName";
+            this.txtDeviceName.Size = new System.Drawing.Size(121, 21);
+            this.txtDeviceName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "设备名";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "设备MAC";
+            // 
+            // txtDeviceMAC
+            // 
+            this.txtDeviceMAC.Location = new System.Drawing.Point(315, 137);
+            this.txtDeviceMAC.Name = "txtDeviceMAC";
+            this.txtDeviceMAC.Size = new System.Drawing.Size(119, 21);
+            this.txtDeviceMAC.TabIndex = 3;
+            // 
+            // dtpUpdateDate
+            // 
+            this.dtpUpdateDate.Location = new System.Drawing.Point(313, 182);
+            this.dtpUpdateDate.MinDate = new System.DateTime(2004, 1, 1, 0, 0, 0, 0);
+            this.dtpUpdateDate.Name = "dtpUpdateDate";
+            this.dtpUpdateDate.Size = new System.Drawing.Size(121, 21);
+            this.dtpUpdateDate.TabIndex = 5;
+            this.dtpUpdateDate.Value = new System.DateTime(2020, 3, 31, 0, 0, 0, 0);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 187);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "同步日期";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dtpUpdateDate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDeviceMAC);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDeviceName);
+            this.Controls.Add(this.btnStartUpdate);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Record Tool";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartUpdate;
+        private System.Windows.Forms.TextBox txtDeviceName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDeviceMAC;
+        private System.Windows.Forms.DateTimePicker dtpUpdateDate;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ pbxrecordtool/Main.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace pbxrecordtool
+{
+    public delegate void LogDebug(string format, params object[] args);
+
+    public partial class Main : Form
+    {
+        static string pbxMgrURL = "http://127.0.0.1:1311";
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dtpUpdateDate.Value = DateTime.Now;
+        }
+
+        private void btnStartUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtDeviceName.Text))
+            {
+                MessageBox.Show("请填写设备名");
+                return;
+            }
+            // test http://127.0.0.1:1311/pbxmgr/device?action=download_records&date=20190211&device_mac=000EA93D209C&device_name=设备名&device_type=2
+            var update = dtpUpdateDate.Value.ToString("yyyyMMdd");
+            string upmac = txtDeviceMAC.Text.ToUpper().Replace(":", "");
+            string url = string.Format("{0}/pbxmgr/device?action=download_records&date={1}&device_mac={2}&device_name={3}&device_type=2", pbxMgrURL, update, upmac, txtDeviceName.Text);
+            string resp = HttpRequest.Post(url, null);
+            MessageBox.Show("请求服务器开始同步录音" + url + " resp " + resp);
+        }
+    }
+
+    public class HttpRequest
+    {
+        public static LogDebug warn = new LogDebug(DefaultLog);
+        private CookieContainer cookie;
+        public HttpRequest()
+        {
+            cookie = new CookieContainer();
+        }
+
+        public static void SetWarn(LogDebug log)
+        {
+            warn = log;
+        }
+
+        public static string Get(string url)
+        {
+            return SendHttpRequest(url, "GET", "");
+        }
+
+        public static string Post(string url, string body)
+        {
+            return SendHttpRequest(url, "POST", body);
+        }
+
+        public static string Put(string url, string body)
+        {
+            return SendHttpRequest(url, "PUT", body);
+        }
+
+        public static string SendHttpRequest(string url, string method, string body)
+        {
+            string msg = "";
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            try
+            {
+                HttpWebRequest req = WebRequest.Create(url) as HttpWebRequest;
+                req.Method = method;
+                req.Accept = "application/xml, text/xml, */*";
+                req.KeepAlive = false;
+                req.Timeout = 10 * 1000;
+                req.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+                if (method.Equals("POST") == true || method.Equals("PUT") == true)
+                {
+                    if (string.IsNullOrEmpty(body) == false)
+                    {
+                        req.ContentLength = Encoding.UTF8.GetByteCount(body);
+                        using (StreamWriter sw = new StreamWriter(req.GetRequestStream()))
+                        {
+                            sw.Write(body);
+                        }
+                    }
+                }
+                using (var res = req.GetResponse())
+                {
+                    Stream respStream = res.GetResponseStream();
+                    using (StreamReader reader = new StreamReader(respStream, Encoding.UTF8))
+                    {
+                        msg = reader.ReadToEnd();
+                    }
+                }
+                warn("{0} {1} success, resp length {2}", method, url, msg.Length);
+            }
+            catch (Exception ex)
+            {
+                warn("{0} {1} fail, {2}", method, url, ex.Message);
+            }
+            return msg;
+        }
+
+        public static void DefaultLog(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+    }
+
+    public class SimpleLog
+    {
+        public static LogDebug LDebug = new LogDebug(DefaultLog);
+        public static LogDebug LWarn = new LogDebug(DefaultLog);
+        public string FileNamePrefix;
+        public string FileName;
+        public int LogLevel;
+        private int rollingSize = 2 * 1024 * 1024;
+        private bool initFail;
+        private FileStream logFs;
+        private StreamWriter logWriter;
+
+        public SimpleLog()
+        {
+            initFail = true;
+        }
+
+        public SimpleLog(string fileNamePrefix)
+        {
+            FileNamePrefix = fileNamePrefix;
+            initLogFile();
+        }
+
+        ~SimpleLog()
+        {
+            try
+            {
+                logWriter.Close();
+                logFs.Close();
+            }
+            catch
+            {
+                return;
+            }
+        }
+
+        public void RollingSize(int size)
+        {
+            if (size > 0)
+            {
+                rollingSize = size;
+            }
+        }
+
+        private void rollingFile()
+        {
+            if (logFs != null && logWriter != null && logFs.Length > rollingSize)
+            {
+                logWriter.Close();
+                logFs.Close();
+                initLogFile();
+            }
+        }
+
+        private void initLogFile()
+        {
+            initFail = false;
+            logWriter = null;
+            try
+            {
+                FileName = string.Format("{0}_{1}.log", FileNamePrefix, DateTime.Now.ToString("yyyyMMdd_HHmmss"));
+                logFs = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Write);
+                logWriter = new StreamWriter(logFs);
+                logWriter.BaseStream.Seek(0, SeekOrigin.End);
+            }
+            catch
+            {
+                initFail = true;
+            }
+        }
+
+        private void prefixLog()
+        {
+            logWriter.Write(string.Format("{0} [{1}] ", DateTime.Now.ToString(), Thread.CurrentThread.ManagedThreadId));
+            //logWriter.Write(logFs.Length);
+        }
+
+        public void Debug(string format)
+        {
+            rollingFile();
+            if (initFail == true || logWriter == null)
+            {
+                return;
+            }
+            prefixLog();
+            logWriter.WriteLine(format);
+            logWriter.Flush();
+        }
+
+        public void Debug(string format, object arg0)
+        {
+            Debug(string.Format(format, arg0));
+        }
+
+        public void Debug(string format, object arg0, object arg1)
+        {
+            Debug(string.Format(format, arg0, arg1));
+        }
+
+        public void Debug(string format, object arg0, object arg1, object arg2)
+        {
+            Debug(string.Format(format, arg0, arg1, arg2));
+        }
+
+        public void Debug(string format, object arg0, object arg1, object arg2, object arg3)
+        {
+            Debug(string.Format(format, arg0, arg1, arg2, arg3));
+        }
+
+        public void Debug(string format, object arg0, object arg1, object arg2, object arg3, object arg4)
+        {
+            Debug(string.Format(format, arg0, arg1, arg2, arg3, arg4));
+        }
+
+        public void Debug(string format, params object[] args)
+        {
+            Debug(string.Format(format, args));
+        }
+
+        public void Info(string format)
+        {
+            Debug(format);
+        }
+
+        public void Info(string format, object arg0)
+        {
+            Debug(string.Format(format, arg0));
+        }
+
+        public void Info(string format, object arg0, object arg1)
+        {
+            Debug(string.Format(format, arg0, arg1));
+        }
+
+        public void Info(string format, object arg0, object arg1, object arg2)
+        {
+            Debug(string.Format(format, arg0, arg1, arg2));
+        }
+
+        public void Warn(string format)
+        {
+            Debug(format);
+        }
+
+        public void Warn(string format, object arg0)
+        {
+            Debug(string.Format(format, arg0));
+        }
+
+        public void Warn(string format, object arg0, object arg1)
+        {
+            Debug(string.Format(format, arg0, arg1));
+        }
+
+        public void Warn(string format, object arg0, object arg1, object arg2)
+        {
+            Debug(string.Format(format, arg0, arg1, arg2));
+        }
+
+        public void Warn(string format, params object[] args)
+        {
+            Debug(string.Format(format, args));
+        }
+
+        /* -- for lib common used -- */
+        public static void DefaultLog(string format, params object[] args)
+        {
+            Console.WriteLine(format, args);
+        }
+    }
+}
